6c0004ef65618b17113b0c2483b17efee6a13e1c
mowerczuk <maciej.owerczuk@gmail.com>
2017-04-03 13:23:58 +0200

    added arrays declarations, extending bits, xor function and not working parsing for blocks, maybe some more

---
+++ DES_Cipher_Logic/Function.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DES_Cipher_Logic
+{
+    public class Function : Constans
+    {
+
+        public string ExtendBits(string inBits)
+        {
+            string outBits = "";
+            
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    outBits += inBits[EBitSelection[i, j] - 1];
+                }
+            }
+
+            return outBits;
+        }
+
+        public string Xor(string input, string key)
+        {
+            string result = "";
+
+            if (input.Length != key.Length)
+            {
+                return null;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ((input[i] == '0' && key[i] == '0') || (input[i] == '1' && key[i] == '1'))
+                {
+                    result += '0';
+                }
+                else
+                {
+                    result += '1';
+                }
+            }
+
+            return result;
+        }
+
+        public string[] ParseFor6bit(string input)
+        {
+            string[] output = new string[8];
+            int byteNumber = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i % 6 == 0)
+                {
+                    byteNumber++;
+                }
+
+                output[byteNumber] += input[i];
+            }
+
+            return output;
+        }
+    }
+}
---
+++ DES_Cipher_Tests/Function_Tests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DES_Cipher_Logic;
+
+namespace DES_Cipher_Tests
+{
+    [TestClass]
+    public class Function_Tests
+    {
+        [TestMethod]
+        public void Function_EBitSelection_Works()
+        {
+            Function f = new Function();
+
+            string result = f.ExtendBits("11110000101010101111000010101010");
+
+            Assert.AreEqual("011110100001010101010101011110100001010101010101", result);
+        }
+
+        [TestMethod]
+        public void Function_Xor_Works()
+        {
+            Function f = new Function();
+
+            string result = f.Xor("011110100001010101010101011110100001010101010101", "000110110000001011101111111111000111000001110010");
+
+            Assert.AreEqual("011000010001011110111010100001100110010100100111", result);
+        }
+
+        [TestMethod]
+        public void Function_ParseFor6bit_Works()
+        {
+            Function f = new Function();
+            string[] result = f.ParseFor6bit("011000010001011110111010100001100110010100100111");
+            string[] expected = new string[8]
+            {
+                "011000",
+                "010001",
+                "011110",
+                "111010",
+                "100001",
+                "100110",
+                "010100",
+                "100111"
+            };
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
